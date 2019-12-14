6bbab7a63b26e8628a60e472e466f7c78a8031e5
Alexander Buczynsky <alexander.buczynsky@siemens.com>
2019-12-13 20:23:14 -0600

    BFS utility added

---
+++ src/utils/bfs/graph-node.ts
@@ -0,0 +1,31 @@
+
+export class GraphNode<T> {
+
+  public value: T;
+
+  /**
+   * The distance to the root node
+   * @alias degree
+   */
+  public distance: number = 0;
+
+  public get childCount(): number {
+    return this.children.length;
+  }
+
+  public visitedBegin: boolean = false;
+
+  public visitedEnd: boolean = false;
+
+  public children: Array<GraphNode<T>>;
+
+  constructor(val: T) {
+    this.value = val;
+    this.children = [];
+  }
+
+  public appendChild(node: GraphNode<T>) {
+    this.children.push(node);
+    return this;
+  }
+}
---
+++ src/utils/bfs/graph.ts
@@ -0,0 +1,84 @@
+import { GraphNode } from './graph-node';
+
+export class Graph<T> {
+
+  public initialized = false;
+
+  protected nodes = new Map<T, GraphNode<T>>();
+
+  public add(val1: T, val2: T) {
+    const a = this.nodes.get(val1) || new GraphNode(val1);
+    const b = this.nodes.get(val2) || new GraphNode(val2);
+
+    this.nodes.set(val1, a.appendChild(b));
+    this.nodes.set(val2, b.appendChild(a));
+  }
+
+  public get(val: T) {
+    return this.nodes.get(val);
+  }
+
+  /**
+   * Calculate degrees of seperation
+   * between two nodes in the graph
+   * network
+   *
+   * @param {T} val
+   * @returns -1 if start or end node does not exist
+   * @memberof GraphNode
+   */
+  public calculateDOS(startVal: T, endVal: T): number {
+
+    let startNode = this.get(startVal);
+    let endNode = this.get(endVal);
+
+    if (!startNode || !endNode) {
+      return -1;
+    }
+
+    startNode.distance = 0;
+    endNode.distance = 1;
+    // startNode.distance = endNode.distance = 1;
+    startNode.visitedBegin = true;
+    endNode.visitedEnd = true;
+
+    const beginQue = [startNode];
+    const endQue = [endNode];
+
+    while (beginQue.length || endQue.length) {
+      startNode = beginQue.shift();
+      endNode = endQue.shift();
+
+      if (startNode === undefined || endNode === undefined) {
+        break;
+      }
+
+      for (const child of startNode.children) {
+
+        if (child.visitedEnd) {
+          return startNode.distance + child.distance;
+        } else if (!child.visitedBegin) {
+          child.distance = startNode.distance + 1;
+          child.visitedBegin = true;
+          beginQue.push(child);
+        }
+
+      }
+
+      for (const child of endNode.children) {
+
+        if (child.visitedBegin) {
+          return endNode.distance + child.distance;
+        } else if (!child.visitedEnd) {
+          child.distance = endNode.distance + 1;
+          child.visitedEnd = true;
+          endQue.push(child);
+        }
+
+      }
+    }
+
+    return 0;
+  }
+
+}
